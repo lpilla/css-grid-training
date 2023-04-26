f14e8e73f44d6172f14f1d1e6bb61681a62e99bb
Lorenzo Pilla <62301813+lpilla@users.noreply.github.com>
2023-04-26 12:57:51 +0200

    update

--- js/main.js
+++ js/main.js
@@ -5,23 +5,22 @@ const industries = document.getElementsByClassName("industry");
 
 for (let i = 0; i < items.length; i++) {
   const element = items[i];
+  const container = document.getElementById("container");
   const hasImage = element.getAttribute("data-image");
   element.addEventListener("mouseover", () => {
-    console.log("fired");
-    if (hasImage) {
-      document.getElementById(
-        "container"
-      ).style.backgroundImage = `url(${element.getAttribute("data-image")})`;
-    } else {
-      document.getElementById(
-        "container"
-      ).style.backgroundImage = `url(./images/gettyimages-1153448444.jpg)`;
-    }
+    hasImage
+      ? (container.style.backgroundImage = `url(${element.getAttribute(
+          "data-image"
+        )})`)
+      : (container.style.background = `url(./images/gettyimages-1153448444.jpg)`);
   });
 }
 
 function seeIndustry() {
   for (let i = 0; i < products.length; i++) {
+    document.getElementById("title").innerText = "FOR ALL MARKETS";
+    document.getElementById("productsController").classList.remove("active");
+    document.getElementById("industryController").classList.add("active");
     const product = products[i];
     const industry = industries[i];
     product.classList.add("hide");
@@ -30,9 +29,14 @@ function seeIndustry() {
 }
 function seeProducts() {
   for (let i = 0; i < products.length; i++) {
+    document.getElementById("title").innerText = "ASIA-BASED GLOBAL SOLUTIONS";
+    document.getElementById("productsController").classList.add("active");
+    document.getElementById("industryController").classList.remove("active");
     const product = products[i];
     const industry = industries[i];
     product.classList.remove("hide");
     industry.classList.add("hide");
   }
 }
+
+seeProducts();
