1d5d54c77043ec5522721ecb78962b197df86f45
Lorenzo Pilla <62301813+lpilla@users.noreply.github.com>
2023-05-24 09:52:48 +0200

    added images

--- js/main.js
+++ js/main.js
@@ -2,25 +2,36 @@ const items = document.getElementsByClassName("item");
 console.log(items);
 const products = document.getElementsByClassName("product");
 const industries = document.getElementsByClassName("industry");
+let activeSelection;
 
 for (let i = 0; i < items.length; i++) {
   const element = items[i];
+
   const container = document.getElementById("container");
-  const hasImage = element.getAttribute("data-image");
+  let child;
   element.addEventListener("mouseover", () => {
-    hasImage
-      ? (container.style.backgroundImage = `url(${element.getAttribute(
-          "data-image"
-        )})`)
-      : (container.style.background = `url(./images/gettyimages-1153448444.jpg)`);
+    if (activeSelection === "products") {
+      child = element.getElementsByClassName("product");
+      const hasImage = child[0].getAttribute("data-image");
+      hasImage
+        ? (container.style.backgroundImage = `url(${hasImage})`)
+        : (container.style.background = `url(./images/1.jpg)`);
+    } else {
+      child = element.getElementsByClassName("industry");
+      const hasImage = child[0].getAttribute("data-image");
+      hasImage
+        ? (container.style.backgroundImage = `url(${hasImage})`)
+        : (container.style.background = `url(./images/1.jpg)`);
+    }
   });
 }
 
 function seeIndustry() {
+  activeSelection = "industry";
+  document.getElementById("title").innerText = "FOR ALL MARKETS";
+  document.getElementById("productsController").classList.remove("active");
+  document.getElementById("industryController").classList.add("active");
   for (let i = 0; i < products.length; i++) {
-    document.getElementById("title").innerText = "FOR ALL MARKETS";
-    document.getElementById("productsController").classList.remove("active");
-    document.getElementById("industryController").classList.add("active");
     const product = products[i];
     const industry = industries[i];
     product.classList.add("hide");
@@ -28,15 +39,15 @@ function seeIndustry() {
   }
 }
 function seeProducts() {
+  activeSelection = "products";
+  document.getElementById("title").innerText = "ASIA-BASED GLOBAL SOLUTIONS";
+  document.getElementById("productsController").classList.add("active");
+  document.getElementById("industryController").classList.remove("active");
   for (let i = 0; i < products.length; i++) {
-    document.getElementById("title").innerText = "ASIA-BASED GLOBAL SOLUTIONS";
-    document.getElementById("productsController").classList.add("active");
-    document.getElementById("industryController").classList.remove("active");
     const product = products[i];
     const industry = industries[i];
     product.classList.remove("hide");
     industry.classList.add("hide");
   }
 }
-
 seeProducts();
